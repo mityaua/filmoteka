04c3388721f42d0a94d5edb9310fcee3808055ef
Vita Khutorianska <steblvita@gmail.com>
2021-03-04 23:57:30 +0200

    create file and import templates

---
+++ src/js/film-modal.js
@@ -0,0 +1,10 @@
+import filmModalInfo from "../js/api/trending-movies";
+import modalFilmTemplate from "../templates/modal-film-detail";
+function openModal(e) {
+    e.preventDefoult();
+     const currentCard = e.target;
+    if (currentFilm.nodeName !== 'IMG') {
+      return;
+    }
+    
+}
