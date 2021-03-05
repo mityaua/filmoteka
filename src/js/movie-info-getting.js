781e8dfc5ad65e90a1fb6572b9f5e758d21d60fa
ragimovalik <ragimovalik@gmail.com>
2021-03-06 00:02:24 +0200

    Gets movie full data by a movie card click on

--- src/js/movie-info-getting.js
+++ src/js/movie-info-getting.js
@@ -1,22 +1,41 @@
+import axios from 'axios';
+import modalTemplate from '../templates/modal-film-detail.hbs';
+
 // ===== Get movie Info by Click =====
 
 //Refs
-
 const collectionBox = document.querySelector('.collection');
-// const collectionItem = () => document.querySelectorAll('.collection__item');
 
-// setTimeout(() => {
-// collectionItem().addEventListener('click', e => {
-//   e.preventDefault();
-//   console.log(e.target);
-//   console.log(e.currentTarget);
-// });
-// }, 500);
+// BaseUrl
+const url = 'https://api.themoviedb.org/3/movie/';
+const apiKey = '249f222afb1002186f4d88b2b5418b55';
 
 //Listener
+collectionBox.addEventListener('click', clickOnMovieHandler);
 
-collectionBox.addEventListener('click', e => {
-  e.preventDefault();
-  console.log(e.target);
-  console.log(e.currentTarget);
-});
+//Functions
+//Click Handler Function
+function clickOnMovieHandler() {
+  event.preventDefault();
+
+  if (event.target.nodeName !== 'IMG' && event.target.nodeName !== 'H2') return;
+  else {
+    let movieId = event.target.dataset.id;
+    getMovieData(movieId).then(renderMovieModal);
+  }
+}
+
+//Fetch Function
+async function getMovieData(movieId) {
+  const { data } = await axios.get(`${url}${movieId}?api_key=${apiKey}`);
+
+  console.log(data);
+  return data;
+}
+
+//Rendering Function
+function renderMovieModal(data) {
+  const modalMarkup = modalTemplate(data);
+
+  console.log(modalMarkup);
+}
