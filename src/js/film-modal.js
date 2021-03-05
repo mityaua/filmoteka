54833beb25c63836e97a423de9ae3b1888433591
Vita Khutorianska <steblvita@gmail.com>
2021-03-05 14:33:01 +0200

    add markup for modal

--- src/js/film-modal.js
+++ src/js/film-modal.js
@@ -1,5 +1,7 @@
 import filmModalInfo from "../js/api/trending-movies";
 import modalFilmTemplate from "../templates/modal-film-detail";
+// import * as basicLightbox from 'basiclightbox';
+// import 'basicLightbox/src/styles/main.scss';
 function openModal(e) {
     e.preventDefoult();
      const currentCard = e.target;
