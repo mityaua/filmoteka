8391f187b807be2b55ee7fe4d21fbbbdaca5770a
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-03-06 12:35:17 +0200

    Added test modal from cdn

--- src/js/render-modal.js
+++ src/js/render-modal.js
@@ -4,5 +4,12 @@ import modalTemplate from '../templates/modal-film-detail.hbs';
 export default function renderMovieModal(data) {
   const modalMarkup = modalTemplate(data);
 
+  // Тестово выводит модалку (убрать при замене на постоянную и убрать в индекс хтмл скрипт с CDN)
+  Swal.fire({
+    html: modalMarkup,
+    showCloseButton: true,
+    showConfirmButton: false,
+  });
+
   console.log(modalMarkup);
 }
