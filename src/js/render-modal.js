0c26bbc8fc274e22f37bcbb4476d00d15d81c21a
ragimovalik <ragimovalik@gmail.com>
2021-03-06 22:26:52 +0200

    In progress - modal closing by ESC & click on backdrop
    Need to add CLOSE BUTTON

--- src/js/render-modal.js
+++ src/js/render-modal.js
@@ -3,7 +3,22 @@ import modalTemplate from '../templates/modal-film-detail.hbs';
 import NProgress from 'nprogress';
 import 'nprogress/nprogress.css';
 
+//Refs for Modal
 const modalBox = document.getElementById('modal');
+
+//Listener for closing by ESC
+window.addEventListener('keydown', modalClosinByEsc);
+
+//Closing functions
+function modalClosing() {
+  modalBox.classList.remove('is-open');
+}
+
+function modalClosinByEsc(event) {
+  if (event.code === 'Escape') {
+    modalClosing();
+  }
+}
 
 // Функция отрисовки модального окна по шаблону
 export default function renderMovieModal(data) {
@@ -15,7 +30,9 @@ export default function renderMovieModal(data) {
     // Выводит модалку
     // modalComponent(modalMarkup);
     modalBox.innerHTML = modalMarkup;
-    modalBox.classList.add('is-open')
+    modalBox.classList.add('is-open');
+    const modalBackdrop = document.querySelector('.modal__backdrop');
+    modalBackdrop.addEventListener('click', modalClosing);
   } catch (error) {
     console.error('Smth wrong with modal window' + error);
   }
