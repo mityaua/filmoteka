63a2817edb8cff59acf5bd1146ae2eb7a8965de5
Dmitriy S <mityaua@users.noreply.github.com>
2021-03-06 18:17:19 +0200

    Update render-modal.js

--- src/js/render-modal.js
+++ src/js/render-modal.js
@@ -10,39 +10,11 @@ export default function renderMovieModal(data) {
   NProgress.start();
 
   try {
-    // Тестово выводит модалку
-
-    Swal.fire({
-      html: modalMarkup,
-      showCloseButton: true,
-      showConfirmButton: false,
-      showClass: {
-        popup: 'animate__animated animate__fadeIn',
-      },
-      customClass: {
-        container: 'modal__container',
-        popup: '...',
-        header: '...',
-        title: '',
-        closeButton: '...',
-        icon: '...',
-        image: 'image',
-        content: '...',
-        htmlContainer: 'modal__container',
-        input: '...',
-        inputLabel: '...',
-        validationMessage: '...',
-        actions: '...',
-        confirmButton: '...',
-        denyButton: '...',
-        cancelButton: '...',
-        loader: '...',
-        footer: '....',
-      },
-    });
+    // Выводит модалку
+    modalComponent(modalMarkup);
   } catch (error) {
     console.error('Smth wrong with modal window' + error);
   }
-
+  
   NProgress.done();
 }
