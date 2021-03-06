366cbc81e36e7d6a2cedc7993377ffc28dc3bf8e
Sergey Glazyrin <glazyrinsb@gmail.com>
2021-03-06 20:23:28 +0200

    Use vars

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './scss/main.scss';
 
 // Скрипты
 import './js/start-page';
+import './js/render-page-library';
 import './js/search';
 import './js/movie-on-modal';
 import './js/components/back-to-top';
