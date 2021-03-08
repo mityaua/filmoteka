08402361caf260fb34e42bbc434352ee5c329171
Dmitriy S <mityaua@users.noreply.github.com>
2021-03-08 22:42:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import 'animate.css/animate.css';
 // Скрипты
 import './js/start-page';
 import './js/render-page-library';
+import './js/my-library';
 import './js/search';
 import './js/movie-on-modal';
 import './js/components/back-to-top';
-import './js/my-library';
