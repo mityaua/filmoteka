aca17603abb3487718192b6dacc18c66d3074c6f
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-03-08 23:01:13 +0200

    fix index js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import 'animate.css/animate.css';
 // Скрипты
 import './js/start-page';
 import './js/render-page-library';
-import './js/my-library';
 import './js/search';
 import './js/movie-on-modal';
 import './js/our-team-modal';
