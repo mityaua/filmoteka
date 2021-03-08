0cb3a14fafabd1af10618646b6b6de9acaba11a0
Vita Khutorianska <steblvita@gmail.com>
2021-03-08 14:50:59 +0200

    modal

--- src/js/our-team-modal.js
+++ src/js/our-team-modal.js
@@ -4,27 +4,26 @@ import teamTemplate from '../templates/our-team.hbs';
 import team from '../team.json';
 
 const modalContainer = document.querySelector('#js-team-modal');
+const btnCloseRef = document.querySelector('button[data-close-modal');
 
 modalContainer.addEventListener('click', openModal);
 function openModal(e) {
   e.preventDefault();
   getTeamInfo(team);
 }
-
-
 function getTeamInfo(teamId) {
   const teamMarkup = teamTemplate(teamId);
   const modalContent = basicLightbox.create(teamMarkup
   );
   modalContent.show();
+  // console.log("modalContent=", modalContent);
   window.addEventListener('keydown', closeModalHandler);
 
   function closeModalHandler(e) {
   if (e.code === 'Escape') {
     modalContent.close();
     window.removeEventListener('keydown', closeModalHandler);
-    }
-
+    };
   }
 }
 
