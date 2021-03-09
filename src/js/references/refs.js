3eaad1605ab574538a6cbc4f91264fceb3b81d5d
Dmitriy S <mityaua@users.noreply.github.com>
2021-03-09 16:18:14 +0200

    Update refs.js

--- src/js/references/refs.js
+++ src/js/references/refs.js
@@ -1,11 +1,9 @@
 const formRef = document.getElementById('search-form');
-// const inputRef = formRef.querySelector('.search__input'); было
-const inputRef = document.querySelector('.search__input'); // стало
+const inputRef = document.querySelector('.search__input');
 const gallery = document.querySelector('.collection');
 const headerRef = document.querySelector('.header');
 const pageHome = document.querySelector('.menu__link-home');
-// const pageLabraryRef = headerRef.querySelector('.menu__link-library'); было
-const pageLabraryRef = document.querySelector('.menu__link-library'); // стало
+const pageLabraryRef = document.querySelector('.menu__link-library');
 const modalBackdrop = document.querySelector('.modal__backdrop');
 const modalBox = document.getElementById('modal');
 const headerWarning = document.querySelector('.warning-notification');
