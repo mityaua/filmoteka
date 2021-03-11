b7d2f5fe7c877584f5b06f7504fef10f4563c901
MarinaKryuchko88 <68333655+MarinaKryuchko88@users.noreply.github.com>
2021-03-11 17:10:29 +0300

    рефакторинг кода темной темы

--- src/js/theme-change.js
+++ src/js/theme-change.js
@@ -3,23 +3,19 @@ import '../templates/film-detail.hbs';
 const bodyRef = document.querySelector('body');
 const toggleRef = document.querySelector('#theme-switch-toggle');
 const footerDarktheme = document.querySelector('.footer');
-const copyrighttDarkTheme = document.querySelector('.footer-text-js');
-const footerTextDarkTheme = document.querySelector('.footer-span-js');
-const footerLinkDarkTheme = document.querySelector('.footer-link-js');
+
 
 toggleRef.addEventListener('change', event => {
   if (bodyRef.classList.contains('dark-theme')) {
     bodyRef.classList.remove('dark-theme');
     bodyRef.classList.add('light-theme');
     footerDarktheme.classList.remove('dark-theme');
-    copyrighttDarkTheme.classList.remove('dark-theme');
-    footerTextDarkTheme.classList.remove('dark-theme');
+   
   } else {
     bodyRef.classList.remove('light-theme');
     bodyRef.classList.add('dark-theme');
     footerDarktheme.classList.add('dark-theme');
-    copyrighttDarkTheme.classList.add('dark-theme');
-    footerTextDarkTheme.classList.add('dark-theme');
+    
   }
 });
 
@@ -27,6 +23,5 @@ export {
   bodyRef,
   toggleRef,
   footerDarktheme,
-  copyrighttDarkTheme,
-  footerTextDarkTheme,
+  
 };
